f1a47db34334e5d21b898c153cac525a82527365
RV <valuhar@gmail.com>
2022-09-08 03:07:47 +0530

    first commit

---
+++ frontend/_app.js
@@ -0,0 +1,6 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import "../styles/globals.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
---
+++ frontend/constants.js
@@ -0,0 +1,5 @@
+export const SUBGRAPH_URL =
+    "https://api.thegraph.com/subgraphs/name/rv12r/celo-nftmarket";
+
+export const MARKETPLACE_ADDRESS = 
+    "0x99B1Fa20f8cbd2458DF5257a68E64aC234729936";
